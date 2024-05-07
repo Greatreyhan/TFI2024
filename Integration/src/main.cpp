9bdc97c2cc6ce3fd9b8b656abc67463f36d14725
greatreyhan <greatreyhan@students.undip.ac.id>
2024-05-07 23:30:52 +0700

    full

--- Integration/src/main.cpp
+++ Integration/src/main.cpp
@@ -1,18 +1,256 @@
 #include <Arduino.h>
+#include "DHT.h"
+#include "time.h"
+// -------------------- NTP Setting ----------------------//
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 3600*7; //GMT+7
+const int   daylightOffset_sec = 3600;
 
-// put function declarations here:
-int myFunction(int, int);
+// -------------------- FIREBASE Setting ----------------------//
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+#define WIFI_SSID "infinergy"
+#define WIFI_PASSWORD "okeokeoke"
+#define API_KEY "AIzaSyBwXYpbgHjGhfrOy6M5IijOP03OMSyJy3A"
+#define DATABASE_URL "https://filtertfi2024-default-rtdb.firebaseio.com" 
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// -------------------- DHT Setting ----------------------//
+#define DHTPIN 4 
+#define DHTTYPE DHT22 
+DHT dht(DHTPIN, DHTTYPE);
+
+// -------------------- MICS Setting ----------------------//
+#include "DFRobot_MICS.h"
+#define CALIBRATION_TIME   3                      
+#define Mics_I2C_ADDRESS MICS_ADDRESS_0
+DFRobot_MICS_I2C mics(&Wire, Mics_I2C_ADDRESS);
+
+// -------------------- CO2 Setting ----------------------//
+#define         MG_PIN                       (13)     //define which analog input channel you are going to use
+#define         BOOL_PIN                     (2)
+#define         DC_GAIN                      (8.5)   //define the DC gain of amplifier
+#define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
+#define         READ_SAMPLE_TIMES            (5)     //define the time interval(in milisecond) between each samples in
+#define         ZERO_POINT_VOLTAGE           (0.220) //define the output of the sensor in volts when the concentration of CO2 is 400PPM
+#define         REACTION_VOLTGAE             (0.030) //define the voltage drop of the sensor when move the sensor from air into 1000ppm CO2
+float           CO2Curve[3]  =  {2.602,ZERO_POINT_VOLTAGE,(REACTION_VOLTGAE/(2.602-3))};
+
+// -------------------- PRIVATE VARIABLE ----------------------//
+// Firebase Variable
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
+
+// DHT Variable
+float humidity = 0;
+float temperature = 0;
+
+// MICS Variable
+float C2H5OHgas = 0;
+float CH4gas = 0;
+float H2gas = 0;
+float NH3gas = 0;
+float COgas = 0;
+float NO2gas = 0;
+
+// CO2 Variable
+int percentageCO2 = 0;
+float voltCO2 = 0;
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  Serial.print("Day of week: ");
+  Serial.println(&timeinfo, "%A");
+  Serial.print("Month: ");
+  Serial.println(&timeinfo, "%B");
+  Serial.print("Day of Month: ");
+  Serial.println(&timeinfo, "%d");
+  Serial.print("Year: ");
+  Serial.println(&timeinfo, "%Y");
+  Serial.print("Hour: ");
+  Serial.println(&timeinfo, "%H");
+  Serial.print("Hour (12 hour format): ");
+  Serial.println(&timeinfo, "%I");
+  Serial.print("Minute: ");
+  Serial.println(&timeinfo, "%M");
+  Serial.print("Second: ");
+  Serial.println(&timeinfo, "%S");
+
+  Serial.println("Time variables");
+  char timeHour[3];
+  strftime(timeHour,3, "%H", &timeinfo);
+  Serial.println(timeHour);
+  char timeWeekDay[10];
+  strftime(timeWeekDay,10, "%A", &timeinfo);
+  Serial.println(timeWeekDay);
+  Serial.println();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+float MGRead(int mg_pin)
+{
+    int i;
+    float v=0;
+
+    for (i=0;i<READ_SAMPLE_TIMES;i++) {
+        v += analogRead(mg_pin);
+        delay(READ_SAMPLE_INTERVAL);
+    }
+    v = (v/READ_SAMPLE_TIMES) *5/1024 ;
+    return v;
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int  MGGetPercentage(float volts, float *pcurve)
+{
+   if ((volts/DC_GAIN )>=ZERO_POINT_VOLTAGE) {
+      return -1;
+   } else {
+      return pow(10, ((volts/DC_GAIN)-pcurve[1])/pcurve[2]+pcurve[0]);
+   }
+}
+
+void readCO2(){
+
+    voltCO2 = MGRead(MG_PIN);
+    Serial.print( "SEN0159:" );
+    Serial.print(volts);
+    Serial.print( "V           " );
+
+    percentageCO2 = MGGetPercentage(voltCO2,CO2Curve);
+    Serial.print("CO2:");
+    if (percentage == -1) {
+        Serial.print( "<400" );
+    } else {
+        Serial.print(percentage);
+    }
+
+    Serial.print( "ppm" );
+    Serial.print("\n");
+
+    if (digitalRead(BOOL_PIN) ){
+        Serial.print( "=====BOOL is HIGH======" );
+    } else {
+        Serial.print( "=====BOOL is LOW======" );
+    }
+
+    Serial.print("\n");
+
+    delay(500);
+}
+
+bool readDHT(){
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return false;
+  }
+  return true;
+}
+
+void readMICS(){
+  C2H5OHgas = mics.getGasData(C2H5OH);
+  CH4gas = mics.getGasData(CH4);
+  H2gas = mics.getGasData(H2);
+  NH3gas = mics.getGasData(NH3);
+  COgas = mics.getGasData(CO);
+  NO2gas = mics.getGasData(NO2);
+}
+
+void sendFirebase(){
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
+      Serial.println("PASSED");
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    count++;
+    
+    // Write an Float number on the database path test/float
+    if (Firebase.RTDB.setFloat(&fbdo, "test/float", 0.01 + random(0,100))){
+      Serial.println("PASSED");
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+}
+
+void setup(){
+
+  Serial.begin(115200);
+
+  // ------------------------- SETUP WIFI -----------------------------//
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  // ------------------------- SETUP MICS -----------------------------//
+  while(!mics.begin()){
+      Serial.println("NO Deivces !");
+      delay(1000);
+    } Serial.println("Device connected successfully !");
+  uint8_t mode = mics.getPowerState();
+  if(mode == SLEEP_MODE){
+    mics.wakeUpMode();
+    Serial.println("wake up sensor success!");
+  }else{
+    Serial.println("The sensor is wake up mode");
+  }
+  while(!mics.warmUpTime(CALIBRATION_TIME)){
+    Serial.println("Please wait until the warm-up time is over!");
+    delay(1000);
+  }
+
+  // ------------------------- SETUP CO2 -----------------------------//
+  pinMode(BOOL_PIN, INPUT);                        //set pin to input
+  digitalWrite(BOOL_PIN, HIGH);                    //turn on pullup resistors
+
+  // ------------------------- SETUP NTP -----------------------------//
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+
+  // ------------------------- SETUP DHT -----------------------------//
+  dht.begin();
+}
+
+void loop(){
+  
 }
